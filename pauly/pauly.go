a3976d1f99ce08223edad48ef4cf8fbc854fc045
Lenfree Yeung <lenfree.yeung@gmail.com>
2016-09-02 15:39:34 +1000

    * Fix broken package path refer to old repo * Fix indentions with vim-go plugin * Fix vsphere package to use New function since there's no initialize method in Go

--- pauly/pauly.go
+++ pauly/pauly.go
@@ -1,99 +1,99 @@
 package pauly
 
 import (
-        "encoding/json"
-        "fmt"
-        "log"
-        "net"
-        "strconv"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"strconv"
 )
 
 type Fields map[string]interface{}
 
 type PaulyLogger struct {
-        Connection  net.Conn
-        Environment string
-        Application string
-        Host        string
-        Port        int
+	Connection  net.Conn
+	Environment string
+	Application string
+	Host        string
+	Port        int
 }
 
 func New(environment, application, host string, port int) PaulyLogger {
-        conn, err := net.Dial("udp", host+":"+strconv.Itoa(port))
-        if err != nil {
-                log.Print("Could not connect to logger", err)
-        }
+	conn, err := net.Dial("udp", host+":"+strconv.Itoa(port))
+	if err != nil {
+		log.Print("Could not connect to logger", err)
+	}
 
-        return PaulyLogger{
-                Connection:  conn,
-                Environment: environment,
-                Application: application,
-                Host:        host,
-                Port:        port,
-        }
+	return PaulyLogger{
+		Connection:  conn,
+		Environment: environment,
+		Application: application,
+		Host:        host,
+		Port:        port,
+	}
 }
 
 func serialize(fields Fields, application, severity string) (string, error) {
-        fields["severity"] = severity
-        fields["application"] = application
-        data, err := json.Marshal(fields)
-        if err != nil {
-                return "", fmt.Errorf("Failed to marshal fields to JSON, %v", err)
-        }
-        return string(data), nil
+	fields["severity"] = severity
+	fields["application"] = application
+	data, err := json.Marshal(fields)
+	if err != nil {
+		return "", fmt.Errorf("Failed to marshal fields to JSON, %v", err)
+	}
+	return string(data), nil
 }
 
 func (pauly *PaulyLogger) Debug(fields Fields) {
-        data, _ := serialize(fields, pauly.Application, "DEBUG")
-        if pauly.Environment == "development" {
-                log.Print(data)
-        }
-        if pauly.Environment == "production" {
-                fmt.Fprintf(pauly.Connection, data)
-        }
+	data, _ := serialize(fields, pauly.Application, "DEBUG")
+	if pauly.Environment == "development" {
+		log.Print(data)
+	}
+	if pauly.Environment == "production" {
+		fmt.Fprintf(pauly.Connection, data)
+	}
 }
 
 func (pauly *PaulyLogger) Info(fields Fields) {
-        data, _ := serialize(fields, pauly.Application, "INFO")
-        if pauly.Environment == "development" {
-                log.Print(data)
-        }
-        if pauly.Environment == "production" {
-                fmt.Fprintf(pauly.Connection, data)
-        }
+	data, _ := serialize(fields, pauly.Application, "INFO")
+	if pauly.Environment == "development" {
+		log.Print(data)
+	}
+	if pauly.Environment == "production" {
+		fmt.Fprintf(pauly.Connection, data)
+	}
 }
 
 func (pauly *PaulyLogger) Warn(fields Fields) {
-        data, _ := serialize(fields, pauly.Application, "WARN")
-        if pauly.Environment == "development" {
-                log.Print(data)
-        }
-        if pauly.Environment == "production" {
-                fmt.Fprintf(pauly.Connection, data)
-        }
+	data, _ := serialize(fields, pauly.Application, "WARN")
+	if pauly.Environment == "development" {
+		log.Print(data)
+	}
+	if pauly.Environment == "production" {
+		fmt.Fprintf(pauly.Connection, data)
+	}
 }
 
 func (pauly *PaulyLogger) Error(fields Fields) {
-        data, _ := serialize(fields, pauly.Application, "ERROR")
-        if pauly.Environment == "development" {
-                log.Print(data)
-        }
-        if pauly.Environment == "production" {
-                fmt.Fprintf(pauly.Connection, data)
-        }
+	data, _ := serialize(fields, pauly.Application, "ERROR")
+	if pauly.Environment == "development" {
+		log.Print(data)
+	}
+	if pauly.Environment == "production" {
+		fmt.Fprintf(pauly.Connection, data)
+	}
 }
 
 func (pauly *PaulyLogger) Fatal(fields Fields) {
-        data, _ := serialize(fields, pauly.Application, "FATAL")
-        if pauly.Environment == "development" {
-                log.Print(data)
-        }
-        if pauly.Environment == "production" {
-                fmt.Fprintf(pauly.Connection, data)
-        }
-        log.Fatal(data)
+	data, _ := serialize(fields, pauly.Application, "FATAL")
+	if pauly.Environment == "development" {
+		log.Print(data)
+	}
+	if pauly.Environment == "production" {
+		fmt.Fprintf(pauly.Connection, data)
+	}
+	log.Fatal(data)
 }
 
-func (pauly *PaulyLogger) Close(){
-        pauly.Connection.Close()
+func (pauly *PaulyLogger) Close() {
+	pauly.Connection.Close()
 }
--- vsphere/vsphere.go
+++ vsphere/vsphere.go
@@ -1,130 +1,139 @@
 package vsphere
 
 import (
-        "net/url"
-        "github.com/vmware/govmomi"
-        "github.com/vmware/govmomi/find"
-        "github.com/vmware/govmomi/object"
-        "github.com/vmware/govmomi/property"
-        "github.com/vmware/govmomi/vim25/mo"
-        "github.com/vmware/govmomi/vim25/types"
-        "golang.org/x/net/context"
-        "strconv"
+	"github.com/vmware/govmomi"
+	"github.com/vmware/govmomi/find"
+	"github.com/vmware/govmomi/object"
+	"github.com/vmware/govmomi/property"
+	"github.com/vmware/govmomi/vim25/mo"
+	"github.com/vmware/govmomi/vim25/types"
+	"golang.org/x/net/context"
+	"net/url"
+	"strconv"
 )
 
 type Config struct {
-        Host     string
-        User     string
-        Pass     string
-        Insecure string
+	Host     string
+	User     string
+	Pass     string
+	Insecure string
 }
 
-func (v Config) Connect(ctx context.Context) (*find.Finder, *govmomi.Client, *object.Datacenter, error){
+func New(host, user, pass, insecure string) Config {
+	return Config{
+		Host:     host,
+		User:     user,
+		Pass:     pass,
+		Insecure: insecure,
+	}
+}
 
-        vcenterURL := "https://" + v.User + ":" + v.Pass + "@" + v.Host + "/sdk"
+func (v Config) Connect(ctx context.Context) (*find.Finder, *govmomi.Client, *object.Datacenter, error) {
 
-        u, _ := url.Parse(vcenterURL)
+	vcenterURL := "https://" + v.User + ":" + v.Pass + "@" + v.Host + "/sdk"
 
-        c, err := govmomi.NewClient(ctx, u, v.insecure())
-        if err != nil {
-                return nil, nil, nil, err
-        }
+	u, _ := url.Parse(vcenterURL)
 
-        f := find.NewFinder(c.Client, true)
+	c, err := govmomi.NewClient(ctx, u, v.insecure())
+	if err != nil {
+		return nil, nil, nil, err
+	}
 
-        // Find one and only datacenter
-        dc, err := f.DefaultDatacenter(ctx)
-        if err != nil {
-                return nil, nil, nil, err
-        }
+	f := find.NewFinder(c.Client, true)
 
-        // Make future calls local to this datacenter
-        f.SetDatacenter(dc)
-        return f, c, dc, nil
+	// Find one and only datacenter
+	dc, err := f.DefaultDatacenter(ctx)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	// Make future calls local to this datacenter
+	f.SetDatacenter(dc)
+	return f, c, dc, nil
 }
 
 func QueryClusterVMs(ctx context.Context, c *govmomi.Client, f *find.Finder, dc *object.Datacenter, p string) ([]mo.VirtualMachine, error) {
 
-        // Assume user pass empty string and default to current directory
-        if len(p) == 0 {
-                p = "*"
-        }
-        // Assume user specify which directory to search for VMs
-        if p != "*" {
-                p = dc.Common.InventoryPath + "/" + "vm" + "/" + p + "/" + "*"
-        }
+	// Assume user pass empty string and default to current directory
+	if len(p) == 0 {
+		p = "*"
+	}
+	// Assume user specify which directory to search for VMs
+	if p != "*" {
+		p = dc.Common.InventoryPath + "/" + "vm" + "/" + p + "/" + "*"
+	}
 
-        cs, err := f.VirtualMachineList(ctx, p)
-        if err != nil {
-                return nil, err
-        }
-        pc := property.DefaultCollector(c.Client)
+	cs, err := f.VirtualMachineList(ctx, p)
+	if err != nil {
+		return nil, err
+	}
+	pc := property.DefaultCollector(c.Client)
 
-        var refs []types.ManagedObjectReference
-        for _, c := range cs{
-                refs = append(refs, c.Reference())
-        }
+	var refs []types.ManagedObjectReference
+	for _, c := range cs {
+		refs = append(refs, c.Reference())
+	}
 
-        var vms []mo.VirtualMachine
-        err = pc.Retrieve(ctx, refs, []string{"summary"}, &vms)
-        if err != nil {
-                return nil, err
-        }
+	var vms []mo.VirtualMachine
+	err = pc.Retrieve(ctx, refs, []string{"summary"}, &vms)
+	if err != nil {
+		return nil, err
+	}
 
-        return vms, nil
+	return vms, nil
 }
 
 func QueryDatastore(ctx context.Context, c *govmomi.Client, f *find.Finder) ([]mo.Datastore, error) {
-        // Find datastores in datacenter
-        dss, err := f.DatastoreList(ctx, "*")
-        if err != nil {
-                return nil, err
-        }
+	// Find datastores in datacenter
+	dss, err := f.DatastoreList(ctx, "*")
+	if err != nil {
+		return nil, err
+	}
 
-        pc := property.DefaultCollector(c.Client)
+	pc := property.DefaultCollector(c.Client)
 
-        var refs []types.ManagedObjectReference
-        for _, ds := range dss {
-                refs = append(refs, ds.Reference())
-        }
+	var refs []types.ManagedObjectReference
+	for _, ds := range dss {
+		refs = append(refs, ds.Reference())
+	}
 
-        var datastores []mo.Datastore
-        err = pc.Retrieve(ctx, refs, []string{"summary"}, &datastores)
-        if err != nil {
-                return nil, err
-        }
+	var datastores []mo.Datastore
+	err = pc.Retrieve(ctx, refs, []string{"summary"}, &datastores)
+	if err != nil {
+		return nil, err
+	}
 
-        return datastores, nil
+	return datastores, nil
 }
 
 func QueryHosts(ctx context.Context, c *govmomi.Client, f *find.Finder) ([]mo.HostSystem, error) {
-        cs, err := f.HostSystemList(ctx, "*")
-        if err != nil {
-                return nil, err
-        }
-        pc := property.DefaultCollector(c.Client)
+	cs, err := f.HostSystemList(ctx, "*")
+	if err != nil {
+		return nil, err
+	}
+	pc := property.DefaultCollector(c.Client)
 
-        var refs []types.ManagedObjectReference
-        for _, c := range cs{
-                refs = append(refs, c.Reference())
-        }
+	var refs []types.ManagedObjectReference
+	for _, c := range cs {
+		refs = append(refs, c.Reference())
+	}
 
-        var hosts []mo.HostSystem
-        err = pc.Retrieve(ctx, refs, []string{"summary"}, &hosts)
-        if err != nil {
-                return nil, err
-        }
-        return hosts, nil
+	var hosts []mo.HostSystem
+	err = pc.Retrieve(ctx, refs, []string{"summary"}, &hosts)
+	if err != nil {
+		return nil, err
+	}
+	return hosts, nil
 }
 
 func (v Config) insecure() bool {
-        if len(v.Insecure) > 0 {
-                b, _ := strconv.ParseBool(v.Insecure)
-                return b
-        }
-        return true
+	if len(v.Insecure) > 0 {
+		b, _ := strconv.ParseBool(v.Insecure)
+		return b
+	}
+	return true
 }
 
 func (v Config) Logout(ctx context.Context) {
-        v.Logout(ctx)
+	v.Logout(ctx)
 }
